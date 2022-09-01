45f270611f05bcb452c625e6fdbd0a6afc34a769
na2na-p <na2na@na2na.dev>
2022-09-01 10:59:07 +0900

    Add DAO基本クラス

---
+++ src/main/java/dev/na2na/trainingjavashopsite/dao/DAO.java
@@ -0,0 +1,18 @@
+package dev.na2na.trainingjavashopsite.dao;
+
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DAO {
+	static DataSource ds;
+
+	public Connection getConnection() throws Exception {
+		if (ds == null) {
+			InitialContext ic = new InitialContext();
+			ds = (DataSource) ic.lookup("java:comp/env/jdbc/trainingjavashopsite");
+		}
+		return ds.getConnection();
+	}
+}
