b08704d52f537ed793881b09515763074d8caa37
na2na-p <na2na@na2na.dev>
2022-09-06 11:31:02 +0900

    WIP

--- src/main/java/dev/na2na/trainingjavashopsite/dao/ProductDAO.java
+++ src/main/java/dev/na2na/trainingjavashopsite/dao/ProductDAO.java
@@ -10,25 +10,24 @@ import dev.na2na.trainingjavashopsite.bean.ProductBean;
 
 public class ProductDAO extends DAO {
 	public List<ProductBean> search(String keyword) throws Exception {
-		List<ProductBean> list= new ArrayList<>();
+		List<ProductBean> list=new ArrayList<ProductBean>();
 
 		Connection con=getConnection();
 
-		try (PreparedStatement st = con.prepareStatement(
-			"select * from product where name like ?")) {
-			st.setString(1, "%" + keyword + "%");
-			ResultSet rs = st.executeQuery();
+		PreparedStatement st=con.prepareStatement(
+			"select * from product where name like ?");
+		st.setString(1, "%"+keyword+"%");
+		ResultSet rs=st.executeQuery();
 
-			while (rs.next()) {
-				ProductBean p = new ProductBean();
-				p.setId(rs.getInt("id"));
-				p.setName(rs.getString("name"));
-				p.setPrice(rs.getInt("price"));
-				list.add(p);
-			}
-
-			st.close();
+		while (rs.next()) {
+			ProductBean p=new ProductBean();
+			p.setId(rs.getInt("id"));
+			p.setName(rs.getString("name"));
+			p.setPrice(rs.getInt("price"));
+			list.add(p);
 		}
+
+		st.close();
 		con.close();
 
 		return list;
