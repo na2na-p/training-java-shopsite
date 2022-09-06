dbf92d6898f82e85d4cec084aacb184b52d48de4
na2na-p <na2na@na2na.dev>
2022-09-06 11:53:05 +0900

    Add EncodingFilter

---
+++ src/main/java/dev/na2na/trainingjavashopsite/util/EncodingFilter.java
@@ -0,0 +1,26 @@
+package dev.na2na.trainingjavashopsite.util;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class EncodingFilter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+	public void destroy() {
+	}
+}
