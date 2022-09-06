d8097baf3d1b9fde0ded1559ecb7fa0b2a2b7074
na2na-p <na2na@na2na.dev>
2022-09-06 11:49:11 +0900

    Update ProductAction

--- src/main/java/dev/na2na/trainingjavashopsite/action/ProductAction.java
+++ src/main/java/dev/na2na/trainingjavashopsite/action/ProductAction.java
@@ -1,4 +1,21 @@
 package dev.na2na.trainingjavashopsite.action;
 
-public class ProductAction {
+import java.util.List;
+import jakarta.servlet.http.*;
+
+import dev.na2na.trainingjavashopsite.bean.ProductBean;
+import dev.na2na.trainingjavashopsite.dao.ProductDAO;
+
+public class ProductAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+		HttpSession session=request.getSession();
+		// keywordを受け取りproductDAOのsearchメソッドでProduct beanのList型を作りセッション属性にセット
+		String keyword=request.getParameter("keyword");
+		ProductDAO productDAO=new ProductDAO();
+		List<ProductBean> list=productDAO.search(keyword);
+		session.setAttribute("productList", list);
+		return "product.jsp";
+	}
 }
